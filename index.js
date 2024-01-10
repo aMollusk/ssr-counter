e5fe7e7fcc620f105044b51288f9de48c1764337
Kieran McDonald <kmd@templafy.com>
2024-01-10 09:38:07 +0100

    Server side rendered counter

---
+++ index.js
@@ -0,0 +1,33 @@
+var fs = require("fs");
+
+const createPage = (link, count) => {
+  return `
+    <!DOCTYPE html>
+    <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+    <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+    <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+    <!--[if gt IE 8]>      <html class="no-js"> <!--<![endif]-->
+    <html>
+        <head>
+            <meta charset="utf-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <title></title>
+            <meta name="description" content="">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <link rel="stylesheet" href="">
+        </head>
+        <body>
+            <!--[if lt IE 7]>
+                <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
+            <![endif]-->
+            <a href="${link}">You've clicked me ${count} times</a>
+            
+        </body>
+    </html>
+    `;
+};
+
+for (let x = 0; x < 100; x++) {
+  const content = createPage(`/${x + 1}.html`, x);
+  fs.writeFileSync(`dist/${x === 0 ? "index" : x}.html`, content, "utf-8");
+}
